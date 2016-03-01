e506b795f5c60d69756aa38584e1667d8f94738c
abhik_setia <abhiksetia@gmail.com>
2016-03-01 23:23:07 +0530

    making GUI of search box

---
+++ CDN.php
@@ -0,0 +1,11 @@
+      <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.12.0/bootstrap-social.min.css">
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+      
+      <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      
+
+      <!--APIS USED-->
+      <!--google plus api-->
+       <script src="https://apis.google.com/js/platform.js" async defer></script>
---
+++ home.php
@@ -0,0 +1,50 @@
+<?php  
+	include 'CDN.php';
+	include 'navbar.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meet Emma</title>
+	<style type="text/css">
+		body,html{
+  			width: 100%;
+  			height: 100%;
+  			padding: 0px;
+  			margin: 0px;
+		}
+		.section_border{
+			border:2px solid black; 
+			height: 100%;
+		}
+	</style>
+</head>
+<body>
+
+<div class="container-fluid" style="margin-top:0px ">
+	<div class="row" style="height: 80%">
+		<div class="col-sm-2 section_border" >
+			<h2> Col-1</h2>
+		</div>
+		<div class="col-sm-8 section_border">
+			<h2> Col-1</h2>
+		</div>
+		<div class="col-sm-2 section_border">
+			<h2> Col-1</h2>
+		</div>
+	</div>
+	<div class="row col-lg-12" style="height: 14%;background-color: #222;width:102%" > 
+		<div class="col-lg-8 col-lg-offset-2" style="margin-top: 2%">
+			<div class="col-sm-10">
+				<input type="text" class="col-sm-8 form-control" placeholder="Say Something">
+			</div>
+			<div>
+				<span class="pull-right col-sm-2"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAACrElEQVR42u2Xz2sTQRSAX8VSb1K8iNqKooJH2Ux6Ksn+iPQqxZMIehJB0do/IMhmQWsvHr2KSEGk0tSLIoWIYNUKij20F2/N7iaUZnYT0kYzzhMKs0HDJiTdLcwHDwKZSd63781LBiQSSW9JZdkhzfKm1Rz9mjZp/W9YdEU3vXv4HsQZ40FtNG36q5rls//Ej4tmbSS2T15Mvp3ExOPmEMQNbBtMMEyoljcFcQN7PqyAlqNfIG7gYQ0tYNIaxA1MrJPY3wImbUqBKAXSFv0tBSIVMOkvKRDtGKWN/T6FdqRAxFNoWwpEPIXqUqBT6ALU/UVgu8GW4GD3f6f9TRDYNJTDrk7YbtiqUumHwIYoUJuHERDAS0r4CvgFECgbY+cFAR7KT+g1POmCKFDNw6WggHc3fBtVb4CAoyauBgXIG+g1Xh5mRAGah6cggBd11fK/h7lOprIs0H6uRl6KAo5O7kOv4QmPiwJ4Jqqv4FiwCtXjvD2+tRmfK6kZ/ygI2HritK0rDVGgrClJ6DWMwYC/AGuCBMYcIC2V0CzvjmbRz3j3xUjn6CfeYreUJ2wQkGD75INPX1mFfsEFrrcIYCvdhC4paWQakxajpJMr0C9YFg54i7AsClRmh9/xnr0NHcInzZStk2aLwAcGMAD9pPIazvFKVDD5rdnhJeHLX5RTyRPQHpz5o66emMc9wdlPtvA8wF7Aq2BUHh1525qEo5JtR1WeOXpickO9cJIpyuD6xJmhYiZ5ytWSl3mlnuOaf+2zDaLDXmJrSgZ/MYVEugo+gSh+FkSBa4yd5Ul87DZ5XpFl/AyIEjzYjkau8WqshU2cr13HPbgX4gJOD97n465GZlyVvC9mSKloKI2iTnbwNT+gBX54H+IaXAtxJzE3ycSAFqSAFJACUkAikXD+AHj5/wx2o5osAAAAAElFTkSuQmCC" alt="microphone_img">
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
