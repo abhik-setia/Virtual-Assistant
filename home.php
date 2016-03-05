c83d4dc9e67854d4ed6efa59ec7b967fe7fa705a
abhik_setia <abhiksetia@gmail.com>
2016-03-05 12:15:58 +0530

    Added power of artificial intelligence and machine learning

--- home.php
+++ home.php
@@ -9,19 +9,214 @@
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
 	<title>Meet Emma</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  	<script type="text/javascript">
+    var accessToken = "c77d14f6105b4faabe0f5a9f251b9c7c",
+      subscriptionKey = "1c55f87c-ecb9-42d0-a789-b846a09cb842",
+      baseUrl = "https://api.api.ai/v1/",
+      $speechInput,
+      $recBtn,
+      recognition,
+      messageRecording = "Recording...",
+      messageCouldntHear = "I couldn't hear you, could you say that again?",
+      messageInternalError = "Oh no, there has been an internal server error",
+      messageSorry = "I'm sorry,It seems that i have'nt learnt that thing yet but will do in some days.";
 
+      $(document).ready(function() {
+        $speechInput = $("#speech");
+        $recBtn = $("#rec");
+
+        $speechInput.keypress(function(event) {
+          if (event.which == 13) {
+            event.preventDefault();
+            send();
+          }
+        });
+          $recBtn.on("click", function(event) {
+            switchRecognition();
+          });
+          $(".debug__btn").on("click", function() {
+            $(this).next().toggleClass("is-active");
+            return false;
+          });
+        });
+      function startRecognition() {
+        recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+            recognition.interimResults = false;
+
+        recognition.onstart = function(event) {
+          respond(messageRecording);
+          updateRec();
+        };
+        recognition.onresult = function(event) {
+          recognition.onend = null;
+          
+          var text = "";
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+              text += event.results[i][0].transcript;
+            }
+            setInput(text);
+          stopRecognition();
+        };
+        recognition.onend = function() {
+          respond(messageCouldntHear);
+          stopRecognition();
+        };
+        recognition.lang = "en-US";
+        recognition.start();
+      }
+      function stopRecognition() {
+        if (recognition) {
+          recognition.stop();
+          recognition = null;
+        }
+        updateRec();
+      }
+
+      function switchRecognition() {
+        if (recognition) {
+          stopRecognition();
+        } else {
+          startRecognition();
+        }
+      }
+
+      function setInput(text) {
+        $speechInput.val(text);
+        send();
+      }
+
+      function updateRec() {
+        $recBtn.text(recognition ? "Stop" : "Speak");
+      }
+
+      function send() {
+        var text = $speechInput.val();
+        $.ajax({
+          type: "POST",
+          url: baseUrl + "query/",
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          headers: {
+            "Authorization": "Bearer " + accessToken,
+            "ocp-apim-subscription-key": subscriptionKey
+          },
+          data: JSON.stringify({q: text, lang: "en"}),
+
+          success: function(data) {
+            prepareResponse(data);
+          },
+          error: function() {
+            respond(messageInternalError);
+          }
+        });
+      }
+
+      function prepareResponse(val) {
+        var debugJSON = JSON.stringify(val, undefined, 2),
+          spokenResponse = val.result.speech;
+
+        respond(spokenResponse);
+        debugRespond(debugJSON);
+      }
+
+      function debugRespond(val) {
+        $("#response").text(val);
+      }
+
+      function respond(val) {
+        if (val == "") {
+          val = messageSorry;
+        }
+
+        if (val !== messageRecording) {
+          var msg = new SpeechSynthesisUtterance();
+          msg.voiceURI = "native";
+          msg.text = val;
+          msg.lang = "en-US";
+          window.speechSynthesis.speak(msg);
+        }
+
+        $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+      }
+      </script>
 	<style type="text/css">
 		body,html{
   			width: 100%;
   			height: 100%;
   			padding: 0px;
   			margin: 0px;
+  			background-color: #192837;
+      		color:#fff;
 		}
 		.section_border{
 			//border:2px solid black; 
 			height: 100%;
 		}
-
+		input {
+		  background-color: #126077;
+		  border: 1px solid #3F7F93;
+		  color: #A6CAE6;
+		  font-family: "Titillium Web";
+		  font-size: 20px;
+		  line-height: 43px;
+		  padding: 0 0.75em;
+		  width: 400px;
+		  -webkit-transition: all 0.35s ease-in;
+		}
+		textarea {
+		  background-color: #070F24;
+		  border: 1px solid #122435;
+		  color: #606B88;
+		  padding: 0.5em;
+		  width: 100%;
+		  -webkit-transition: all 0.35s ease-in;
+		}
+		input:active, input:focus, textarea:active, textarea:focus {
+		  outline: 1px solid #48788B;
+		}
+		.debug__content {
+		  font-size: 14px;
+		  max-height: 0;
+		  overflow: hidden;
+		  -webkit-transition: all 0.35s ease-in;
+		}
+		.debug__content.is-active {
+		  display: block;
+		  max-height: 500px;
+		}
+		.btn:hover {
+		  background-color: #1888A9;
+		  color: #183035;
+		}
+		.spoken-response {
+		  max-height: 0;
+		  overflow: hidden;
+		  -webkit-transition: all 0.35s ease-in;
+		}
+		.spoken-response.is-active {
+		  max-height: 400px;
+		}
+		.spoken-response__text {
+		  background-color: #040E23;
+		  color: #7584A2;
+		  padding: 1em;
+		}
+		.btn {
+		  background-color: #126178;
+		  border: 1px solid #549EAF;
+		  color: #549EAF;
+		  cursor: pointer;
+		  display: inline-block;
+		  font-family: "Titillium Web";
+		  font-size: 20px;
+		  line-height: 43px;
+		  padding: 0 0.75em;
+		  text-align: center;
+		  text-transform: uppercase;
+		  -webkit-transition: all 0.35s ease-in;
+		}
 	</style> 
 </head>
 <body>
@@ -31,9 +226,27 @@
 		<div class="col-sm-2 section_border" >
 			<?php include'includes/sidebar.php'; ?>
 		</div>
-		<div class="col-sm-7 section_border">
-			<h2> Col-1</h2>
-		</div>
+		<div class="col-sm-7 section_border" style="font-family: "Titillium Web", Arial, sans-serif;">
+<!-- 			<iframe src="material_search.html" style="width: 100%;height: 100%"></iframe>
+ -->		<input id="speech" type="text">	
+ 			<button id="rec" class="btn">Speak</button>
+ 		<div class="spoken-response" id="spokenResponse">
+ 			<div class="spoken-response__text">
+ 				
+ 			</div>
+ 		</div>
+
+ 		<div class="debug">
+ 			<div class="debug__btn btn">
+ 				DEBUG JSON RESULTS
+ 			</div>
+ 		</div>
+ 		<div class="debug__content">
+ 			<textarea id="response" cols="40" rows="20">
+ 				
+ 			</textarea>
+ 		</div>
+ 		</div>
 		<div class="col-sm-3 section_border"
 		 style="background-image:url('http://cache4.asset-cache.net/xd/505229569.jpg?v=1&c=IWSAsset&k=2&d=62CA815BFB1CE48053E4E3C9F9FFA16F86871D979DBBA836C62C6C731A6616AADB1EAF54E7E3BE9A'); background-size:cover; color:#fff;">
   <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
--- includes/CDN.php
+++ includes/CDN.php
@@ -1,6 +1,7 @@
       <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/4.12.0/bootstrap-social.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Titillium+Web:200" rel="stylesheet" type="text/css">
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
       
       <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
